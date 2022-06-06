130c936beca518e100256ff0cfd7e6ed19a59b27
Simon3640 <simon.garcial@udea.edu.co>
2022-06-06 10:36:25 -0500

    Se avanzo en el front-end, se agregó el panel de productos y el popup de anadir producto

---
+++ Restaurapp-frontend/src/app/components/pages/panel-de-control/product-panel/product-panel.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductPanelRoutingModule } from './product-panel-routing.module';
+import { ProductPanelComponent } from './product-panel.component';
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    ProductPanelRoutingModule
+  ]
+})
+export class ProductPanelModule { }
